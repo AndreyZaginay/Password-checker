5e3fa38e11ad196fcd00a99ff6d463e3eaec6839
Andrey <andrey201223@ukr.net>
2023-11-09 05:40:45 +0200

    added routing and angular material

---
+++ frontend/src/app/password-checker/password-checker.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-password-checker',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './password-checker.component.html',
+  styleUrl: './password-checker.component.scss'
+})
+export class PasswordCheckerComponent {
+
+}
