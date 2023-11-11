4ea37f8ca0e902bdf1ecee1e12b78a674cbb7dc6
Andrey <andrey201223@ukr.net>
2023-11-11 17:41:24 +0200

    added styles and spreded custom validator

--- frontend/src/app/password-checker/password-checker.component.ts
+++ frontend/src/app/password-checker/password-checker.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { easyPasswordValidator, mediumPasswordValidator, strongPasswordValidator } from './validators/password.validator';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-password-checker',
@@ -16,17 +19,37 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './password-checker.component.html',
   styleUrl: './password-checker.component.scss'
 })
-export class PasswordCheckerComponent implements OnInit {
+export class PasswordCheckerComponent implements OnInit, OnDestroy {
+ 
+  form!: FormGroup;
+  formChangesSub!: Subscription;
+  allRed = false;
+  allGreen = false;
+  firstRed = false;
+  bothYellow = false;
 
-  passwordForm!: FormGroup;
+
+  get password(): AbstractControl {
+    return this.form.get('password') as FormControl;
+  }
 
   ngOnInit(): void {
     this.initPasswordForm();
-  }  
+    this.formChangesSub = this.form.valueChanges.subscribe(({ password }) => {
+      this.allRed = this.password.hasError('minlength');
+      this.firstRed = this.password.hasError('easy');
+      this.bothYellow = this.password.hasError('medium');
+      this.allGreen = this.password.hasError('strong');
+    });
+  } 
 
   initPasswordForm(): void {
-    this.passwordForm = new FormGroup({
-      password: new FormControl(null)
+    this.form = new FormGroup({
+      password: new FormControl(null, [Validators.minLength(8), easyPasswordValidator, mediumPasswordValidator, strongPasswordValidator])
     })
+  }
+
+  ngOnDestroy(): void {
+    this.formChangesSub.unsubscribe();
   }
 }
--- frontend/src/app/password-checker/validators/password.validator.ts
+++ frontend/src/app/password-checker/validators/password.validator.ts
@@ -1,6 +1,17 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+import { AbstractControl, ValidationErrors } from "@angular/forms";
 
-export const matchPassword: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-    const password = control.get('password');
-    return null;
-} 
+export function easyPasswordValidator(control: AbstractControl): ValidationErrors | null {
+    const easy = /^\d+$|^[a-z]+$|^[\W_]+$/gi.test(control.value);
+    return easy ? { easy: { value: control.value } } : null;
+}
+  
+export function mediumPasswordValidator(control: AbstractControl): ValidationErrors | null {
+    const medium = /^(?=.*\d)(?=.*?[a-z])[a-z\d]+$|^(?=.*\d)(?=.*?[\W_])[\d\W_]+$|^(?=.*[\W_])(?=.*?[a-z])[a-z\W_]+$/gi.test(control.value);
+    return medium ? { medium: { value: control.value } } : null;
+}
+  
+export function strongPasswordValidator(control: AbstractControl): ValidationErrors | null {
+    const strong = /^(?=.*\d)(?=.*[a-z])(?=.*[\W_])[a-z\d\W_]+$/gi.test(control.value);
+    return strong ? { strong: { value: control.value } } : null;
+}
+  
